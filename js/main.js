08916d94e99182881e8a07466034fbcfe58bd078
Omar Gonzales <oma.gonzales@gmail.com>
2017-10-17 09:19:40 -0500

    Softlab web 0.1

---
+++ js/main.js
@@ -0,0 +1,9 @@
+
+    $( document ).ready(function() {
+        console.log( "document loaded" );
+    });
+ 
+    $( window ).on( "load", function() {
+        console.log( "window loaded" );
+        alert("Hola!");
+    });
